e46808cc1297e0d8f15e72a27a77a07bcbe671b3
gutii <gutii@RIKITAV>
2026-06-28 04:05:51 +0400

    * workflow fix

--- ShardScript.NET/ShardScriptAPI.cs
+++ ShardScript.NET/ShardScriptAPI.cs
@@ -1,10 +1,11 @@
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace ShardScript;
 
 internal static class ShardScriptAPI
 {
-    public const string LibraryName = "ShardScript.dll";
+    public const string LibraryName = "ShardScript";
 
     public static string Version
     {
@@ -13,6 +14,28 @@ internal static class ShardScriptAPI
             IntPtr ptr = NativeMethods.Shard_GetVersion();
             return ptr == IntPtr.Zero ? string.Empty : Marshal.PtrToStringUni(ptr)!;
         }
+    }
+
+    static ShardScriptAPI()
+    {
+        NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), DllImportResolver);
+    }
+
+    private static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        if (libraryName == LibraryName)
+        {
+            string actualName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? "ShardScript.dll"
+                : "libShardScript.so";
+
+            if (NativeLibrary.TryLoad(actualName, assembly, searchPath, out IntPtr handle))
+            {
+                return handle;
+            }
+        }
+
+        return IntPtr.Zero;
     }
 
     private static class NativeMethods
