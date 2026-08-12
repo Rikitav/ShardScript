926af82837f0d6c472adbcfd2468fd870a33f0ca
eugene <evgenijantonenkov456@gmail.com>
2026-08-12 14:48:07 +0300

    fix(cli): reset argument parser flags before parsing

--- ShardScript.Interpreter/utilities/ArgumentsParser.cpp
+++ ShardScript.Interpreter/utilities/ArgumentsParser.cpp
@@ -63,6 +63,9 @@ static std::vector<std::wstring> ExpandGlob(const std::wstring& pattern)
 void shard::ShardUtilities::ParseArguments(int argc, wchar_t* argv[])
 {
     ConsoleArguments::RunProgram = false;
+    ConsoleArguments::UseInteractive = false;
+    ConsoleArguments::ShowHelp = false;
+    ConsoleArguments::ShowDecompile = false;
 
     for (int i = 1; i < argc; ++i)
     {
